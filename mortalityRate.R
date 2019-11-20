53a02856d02400993c91b279cd652f44596d12c1
Lacey Jeroue <lacey.jeroue@gmail.com>
2019-11-20 14:55:17 -0700

    fixed bug in subset processing

--- mortalityRate.R
+++ mortalityRate.R
@@ -2,18 +2,20 @@ mortalityRate <- function(
   inputData,
   gutRemoval,
   shotConversion,
-  locations = FALSE,
+  locations = NULL, # a character vector with Location names if a subset is desired.
+  col_locationName = NULL,
   num_iters = 5000,
   alpha_levels = seq(0, 1, 0.1)){
   
   
   # Allow calculation over a select subset of locations from original dataset
-  if(locations){
-    regions <- inputData$loc_index[inputData[col_locationName] %in% locations]
+  if(length(locations) > 0){
+    regions <- subset(inputData, get(col_locationName) %in% locations)$loc_index
   } else {
     regions <- inputData$loc_index
   }
   
+  num_loc <- length(regions)
   
   mortEagle_gut <- array(0, dim=c(num_iters, num_loc, length(alpha_levels)))
   mortEagle_shot <- array(0, dim=c(num_iters, num_loc, length(alpha_levels)))
@@ -22,22 +24,22 @@ mortalityRate <- function(
   
   
   total_eagle = NULL
-  for(m in regions){   #RUNS
-    loc_index = which(inputData$loc_index == regions[m])
-    eagle_sum =  inputData$total_eagles[inputData$loc_index == regions[m]] #total eagle at unit m
-    total_eagle = rbind(total_eagle, eagle_sum)   
+  for(m in 1:length(regions)){   
+    loc_index <- which(inputData$loc_index == regions[m])
+    eagle_sum <-  inputData$total_eagles[inputData$loc_index == regions[m]] #total eagle at unit m
+    total_eagle <- rbind(total_eagle, eagle_sum)   
     for(i in 1:num_iters){
       for(a in 1:length(alpha_levels)){
-        mortEagle_gut[i,m,a] = drop(t(gutRemoval[i,loc_index,a,1])) * eagle_sum
-        mortEagle_shot[i,m,a] = drop(t(shotConversion[i,loc_index,a,1])) * eagle_sum
+        mortEagle_gut[i, m, a] = drop(t(gutRemoval[i, loc_index, a, 1])) * eagle_sum
+        mortEagle_shot[i, m, a] = drop(t(shotConversion[i, loc_index, a, 1])) * eagle_sum
       } # end
     }# end
   }# end
   
-  for(i in 1:num_iters){      #RUNS
+  for(i in 1:num_iters){     
     for(a in 1:length(alpha_levels)){
-      sum_mortEagle_gut[i,a] = sum(mortEagle_gut[i,,a])
-      sum_mortEagle_shot[i,a] = sum(mortEagle_shot[i,,a])
+      sum_mortEagle_gut[i,a] <- sum(mortEagle_gut[i,,a])
+      sum_mortEagle_shot[i,a] <- sum(mortEagle_shot[i,,a])
     }# end
   }# end 
   
